301caf7298b08622f983700760a28881afd45e3d
kassimiro <gustavo.cassimiro26@gmail.com>
2020-07-20 15:42:53 -0300

    aaaaaa

--- funcoes/gerador_html_v3.py
+++ funcoes/gerador_html_v3.py
@@ -5,6 +5,8 @@ def tag_bloco(texto, classe='success', inline=False):
 
 
 def tag_lista(*itens):
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
+    return f'<ul>{lista}</ul>'
 
 
 if __name__ == "__main__":
@@ -12,3 +14,4 @@ if __name__ == "__main__":
     print(tag_bloco('inline e classe', 'info', True))
     print(tag_bloco('inline', inline=True))
     print(tag_bloco('falhou', inline=True, classe='error'))
+    print(tag_bloco(tag_lista('item 1', 'item 2'), classe='info'))
