99bcf225ddb792a277c2993b9d871d8f05506ea1
kassimiro <gustavo.cassimiro26@gmail.com>
2020-07-23 15:12:38 -0300

    27/07/2020

--- funcoes/problema_arg_padrao_mutavel.py
+++ funcoes/problema_arg_padrao_mutavel.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+def fibonacci(sequencia=[0, 1]):
+    # Uso de mutaveis como valor default (armadilha)
+    sequencia.append(sequencia[-1] + sequencia[-2])
+    return sequencia
+
+
+if __name__ == "__main__":
+    inicio = fibonacci()
+    print(inicio, id(inicio))
+    print(fibonacci(inicio))
+    restart = fibonacci()
+    print(restart, id(restart))
+
+# A *LISTA* "SEQUENCIA", POR SER *MUTAVEL*, AO LONGO DO PROCESSO
+# É ALTERADA MUDANDO O VALOR DEFAULT (PADRAO) QUE DEVERIA SER
+# ESTATICO
+
+# UMA DAS POSSIVEIS SOLUÇÕES: USAR *TUPLA()* NO LUGAR DE *LISTA[]*
