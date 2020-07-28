ab5eed9b0e820494c70149ee42ddb1472dec16cd
kassimiro <gustavo.cassimiro26@gmail.com>
2020-07-28 15:09:36 -0300

    28/07/2020

--- exercicios/empresa_wyx/exercicio_2.py
+++ exercicios/empresa_wyx/exercicio_2.py
@@ -1,7 +1,20 @@
-from funcoes import func
+def calc_horas(hora, falta):
+    return hora_extra - 2/3*(hora_extra - hora_falta)
+
+
+def consulta(media):
+    if media > 2400:
+        return print('Premio: R$ 1500')
+    elif media > 1800 and media < 2399:
+        return print('Premio: R$ 1000')
+    elif media > 1200 and media < 1799:
+        return print('Premio: R$ 890')
+    elif media < 1200:
+        return print('Premio: R$ 500')
+
 
 hora_extra = float(input('Horas extras prestadas: '))
 hora_falta = float(input('Horas faltadas: '))
 
-h = func.calc_horas(hora_extra, hora_falta)
-func.consulta(h)
+h = calc_horas(hora=hora_extra, falta=hora_falta)
+consulta(h)
