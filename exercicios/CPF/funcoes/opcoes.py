575b35e7349d7639a7cf7d09c13515df82223d7d
kassimiro <gustavo.cassimiro26@gmail.com>
2020-07-29 13:33:05 -0300

    29/07/2020

--- exercicios/CPF/funcoes/opcoes.py
+++ exercicios/CPF/funcoes/opcoes.py
@@ -1,18 +1,37 @@
-from .sub_funcoes import gerador
+from .sub_funcoes import gerador, verificador, cidade
 
 
 def menus(estado):
     estado()
 
+# GERAR DOIS ULTIMOS DIGITOS DO CPF A PARTIR
+# DOS NUMEROS DE ENTRADA ESCOLHIDOS PELO USUARIO
+
 
 def estado_1():
+    print('')
     cpf_inicial = input('Digite o número do CPF sem os 2 últimos dígitos: \n')
     gerador.calc(cpf=cpf_inicial)
 
 
+# VERIFICAR SE O CPF É VALIDO
+# (COM BASE NOS REQUISITOS PROPOSTOS)
+
+
 def estado_2():
-    print('estado2')
+    print('')
+    cpf_completo = input('Digite o numero do CPF completo:')
+    verificador.check(cpf=cpf_completo)
+
+
+# VERIFICAR ESTADO(LOCAL) DE ORIGEM
 
 
 def estado_3():
-    print('estado3')
+    print('')
+    cpf_completo = input('Digite o numero do CPF completo:')
+    digitos = verificador.check(cpf=cpf_completo)
+    if digitos != 0:
+        cidade.check_cidade(cpf=digitos)
+    else:
+        print('** CPF INVALIDO! **')
---
+++ exercicios/CPF/funcoes/sub_funcoes/cidade.py
@@ -0,0 +1,2 @@
+def check_cidade(cpf):
+    pass
