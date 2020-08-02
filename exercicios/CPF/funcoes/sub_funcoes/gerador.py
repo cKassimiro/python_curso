1b27d6430ebfd60c8776c1632eb2073f7f530f7a
cKassimiro <dihogo.teixeira@gmail.com>
2020-08-02 17:26:00 -0300

    building gerador

--- exercicios/CPF/funcoes/sub_funcoes/gerador.py
+++ exercicios/CPF/funcoes/sub_funcoes/gerador.py
@@ -5,14 +5,13 @@ def calc(cpf):
     verif = int(soma_mult[0])*10 + int(soma_mult[1])*9 + int(soma_mult[2])*8\
         + int(soma_mult[3])*7 + int(soma_mult[4])*6 + int(soma_mult[5])*5\
         + int(soma_mult[6])*4 + int(soma_mult[7])*3 + int(soma_mult[8])*2
-    div = verif * (10/11)
-    print(div)
-    print('Valor:', verif)
+    rest_div = verif % 11
+    final = rest_div - 11
+    if final > 9:
+        digi1 = 0
+        print(f'primeiro digito: {digi1}')
+    else:
+        digi1 = final
+        print(f'primeiro digito: {digi1}')
 
-    # cpf_num = cpf
-    # soma_digi1 = 0
-    # for chave, multiplicador in enumerate(range(len(cpf_num)+1, 1, -1)):
-    #     print(f'multiplicador: {multiplicador}')
-    #     print(f'chave: {chave}')
-    #     soma_digi1 += int(cpf_num[chave])*multiplicador
-    # print(soma_digi1)
+    print('Valor:', verif)
