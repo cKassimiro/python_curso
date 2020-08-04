2908b3985d01388f6554b1089925bb763919de78
cKassimiro <gustavo.cassimiro26@gmail.com>
2020-08-04 16:10:33 -0300

    P.O.O

---
+++ poo/data_v1.py
@@ -0,0 +1,16 @@
+class Data:
+    pass
+
+
+d1 = Data()
+d1.data = 26
+d1.mes = 3
+d1.ano = 2001
+
+d2 = Data()
+d2.data = 4
+d2.mes = 8
+d2.ano = 2020
+
+print(f'{d1.data}/{d1.mes}/{d1.ano}')
+print(f'{d2.data}/{d2.mes}/{d2.ano}')
